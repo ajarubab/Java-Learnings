a3c08e9c7ba35c6fe961afc3ab46a3ddc02a590f
RKP <ajarubab@gmail.com>
2024-08-10 22:56:03 +0530

    Java leanings : Hello world, Shortcuts and Major basic datatypes

---
+++ JL_01_Hello_World.java
@@ -0,0 +1,30 @@
+public class JL_01_Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        /*
+         * IntelliJ Idea Keyboard Shortcuts
+         *
+         *   https://www.jetbrains.com/help/idea/reference-keymap-mac-default.html#global_vcs_actions
+         *
+         *   1.  Move Line Up : Opt + Shift + Up Arrow
+         *   2.  Move Line Down : Opt + Shift + Down Arrow
+         *   3.  Delete the line : Command + Delete
+         *   4.  Re-Format Code : Command + Option + L
+         *   5.  Select All Occurrence : Command + Control + G
+         *   6.  Clone cursor carat up : double Option + up
+         *   7.  Clone cursor carat down : double Option + down
+         *   8.  Clone cursor caret at specific place : control + shift + click
+         *   9.  Shrink all : command + shift - minus
+         *   10. Expand all : command + shift + plus
+         *   11. Shrink current module : command + minus
+         *   12. Expand current module : command + plus
+         *   13. Find and get next Occurrence : command + F then command + G .....
+         *   14. Universal Search : double Shift
+         *   15. Build : command + F9
+         *   16. Rebuild : command + Shift + F9
+         *   17. Run : control + option + R
+         *   18. Debug : control + option + D
+         */
+    }
+}
---
+++ JL_02_Variables.java
@@ -0,0 +1,26 @@
+public class JL_02_Variables {
+    public static void main(String[] args) {
+
+        boolean myBoolean = true;               // 1 bit
+        byte myByte = 101;                      // 1 byte
+        char myChar = 'A';                      // 2 bytes
+        short myShort = 32000;                  // 2 bytes
+        int myInt = 1234567890;                 // 4 bytes
+        float myFloat = 244.452f;               // 4 bytes
+        long myLong = 1234567890123L;           // 8 bytes
+        double myDouble = 274651276451.63457D;  // 8 bytes
+        String myStr = "Raja";
+
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+        System.out.println(myChar);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myFloat);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myStr);
+
+
+    }
+}
